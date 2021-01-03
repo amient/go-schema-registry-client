6b74e2d56adebc12085652a4b11a295c8e508f32
Michal Harish <michal.harish@gmail.com>
2021-01-03 14:20:09 +0000

    add tls config helper methods

--- config.go
+++ config.go
@@ -1,6 +1,12 @@
 package schema_registry
 
-import "crypto/tls"
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+)
 
 type LogLevel uint32
 
@@ -31,4 +37,61 @@ func (c *Config) LogCaches() bool {
 
 func (c *Config) LogEntities() bool {
 	return c.LogLevel & LogEntities > 0
+}
+
+func (c *Config) AddCaCert(caPemFile string) error {
+	if c.Tls == nil {
+		c.Tls = new(tls.Config)
+	}
+	if c.Tls.RootCAs == nil {
+		c.Tls.RootCAs = x509.NewCertPool()
+	}
+	caCert, err := ioutil.ReadFile(caPemFile)
+	if err != nil {
+		return err
+	}
+	if ok := c.Tls.RootCAs.AppendCertsFromPEM(caCert); !ok {
+		return fmt.Errorf("could not add root ca from %v", caPemFile)
+	}
+	return nil
+}
+
+func (c *Config) AddClientCert(certFile, keyFile, keyPass string) error {
+
+	if c.Tls == nil {
+		c.Tls = new(tls.Config)
+	}
+	var cert tls.Certificate
+	certBlock, err := ioutil.ReadFile(certFile)
+	if err != nil {
+		return err
+	}
+	pemData, err := ioutil.ReadFile(keyFile)
+	if err != nil {
+		return err
+	}
+	v, _ := pem.Decode(pemData)
+	if v == nil {
+		return fmt.Errorf("no RAS key found in file: %v", keyFile)
+	}
+	if v.Type == "RSA PRIVATE KEY" {
+		var pkey []byte
+		if x509.IsEncryptedPEMBlock(v) {
+			pkey, _ = x509.DecryptPEMBlock(v, []byte(keyPass))
+			pkey = pem.EncodeToMemory(&pem.Block{
+				Type:  v.Type,
+				Bytes: pkey,
+			})
+		} else {
+			pkey = pem.EncodeToMemory(v)
+		}
+		if cert, err = tls.X509KeyPair(certBlock, pkey); err != nil {
+			return err
+		}
+	} else {
+		return fmt.Errorf("only RSA private keys in PEM form are supported, got: %q", v.Type)
+	}
+	c.Tls.Certificates = []tls.Certificate{cert}
+
+	return nil
 }
